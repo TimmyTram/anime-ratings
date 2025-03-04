ab66392f15a1f405132ee493b86e812b3f2c30ae
Timmy Tram <74998146+TimmyTram@users.noreply.github.com>
2025-03-03 16:40:44 -0800

    refactored useEffects into hooks

--- app/components/layout/RankResults.tsx
+++ app/components/layout/RankResults.tsx
@@ -1,36 +1,13 @@
 'use client';
 
-import { useState, useEffect } from 'react';
-import { useRouter, useSearchParams } from 'next/navigation';
-import { AnimeData } from '../../types/AnimeData';
 import ContentGrid from './ContentGrid';
 import AnimeCard from '../anime/AnimeCard';
 import Pagination from '../pagination/Pagination';
+import useRankResults from '@/app/hooks/jikan/useRankResults';
 
 const RankResults = () => {
-    const router = useRouter();
-    let initialPage = 1;
-    const searchParams = useSearchParams();
-    initialPage = parseInt(searchParams.get('page') || '1', 10);
-    const [currentPage, setCurrentPage] = useState(initialPage);
-    const limit = 8;
-    const [animeList, setAnimeList] = useState<AnimeData[]>([]);
-    const [totalPages, setTotalPages] = useState(1);
-
-    useEffect(() => {
-        fetch(`https://api.jikan.moe/v4/top/anime?page=${currentPage}&limit=${limit}`)
-            .then(response => response.json())
-            .then(data => {
-                setAnimeList(data.data);
-                setTotalPages(data.pagination.last_visible_page);
-            });
-    }, [currentPage]);
-
-    const handlePageChange = (page: number) => {
-        setCurrentPage(page);
-        router.push(`?page=${page}`, { scroll: false });
-    };
-
+    const { animeList, currentPage, totalPages, handlePageChange } = useRankResults(8); // Fetch 8 results per page
+    
     return (
         <div>
             <ContentGrid loading={false} error={null}>
--- app/components/ui/Searchbar.tsx
+++ app/components/ui/Searchbar.tsx
@@ -1,40 +1,16 @@
 'use client';
 
 import SearchIcon from '../icons/Search.icon';
-import { useRouter } from 'next/navigation';
-import useFetchAnimeByName from '../../hooks/jikan/useFetchAnimeByName';
-import { useSearchContext } from '@/app/context/SearchContextProvider';
 
-const Searchbar = () => {
-    const { 
-        searchTerm, 
-        setSearchTerm, 
-        currentPage,
-        setCurrentPage,
-        setAnimeList,  
-        setPagination  
-    } = useSearchContext();
-    const router = useRouter();
-    const { fetchAnimeByName } = useFetchAnimeByName();
+interface SearchbarProps {
+    searchTerm: string;
+    setSearchTerm: (searchTerm: string) => void;
+}
 
-    const handleSubmit = async (e: React.FormEvent) => {
-        e.preventDefault();
-        if (searchTerm === '') return;
-        setCurrentPage(1);
-        const status = await fetchAnimeByName(searchTerm, currentPage);
-        if (status.success) {
-            setAnimeList(status.animeList);
-            setPagination(status.pagination);
-            router.push(`/search?q=${searchTerm}&page=${currentPage}`, undefined);
-        }
-    };
-
+const Searchbar: React.FC<SearchbarProps> = ({ searchTerm, setSearchTerm }) => {
     return (
-        <div className="flex items-center justify-center px-8">
-            <form
-                onSubmit={handleSubmit}
-                className="flex flex-row w-full bg-secondary text-white border-4 border-white p-6 focus:outline-white rounded-full shadow-md placeholder-white"
-            >
+        <div className="p-16">
+            <div className="flex flex-row w-full bg-secondary text-white border-4 border-white p-6 focus:outline-white rounded-full shadow-md placeholder-white">
                 <input
                     type="text"
                     placeholder="Search for anime"
@@ -44,7 +20,7 @@ const Searchbar = () => {
                     autoComplete="off"
                 />
                 <SearchIcon size={24} className="ml-auto" />
-            </form>
+            </div>
         </div>
     );
 };
