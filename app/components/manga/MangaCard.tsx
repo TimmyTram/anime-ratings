6d80b78d1a00cc90668b222ce36b77dcb53c1949
Timmy Tram <74998146+TimmyTram@users.noreply.github.com>
2025-03-04 13:22:55 -0800

    turning common into reusable components

--- app/components/manga/MangaCard.tsx
+++ app/components/manga/MangaCard.tsx
@@ -1,6 +1,6 @@
 import { MangaData } from '../../types/MangaData';
-import Image from 'next/image';
-import MangaCardClickable from './MangaCardClickable';
+import ClickableCard from '../shared/ClickableCard';
+import Card from '../shared/Card';
 
 type MangaCardProps = {
     manga: MangaData;
@@ -8,35 +8,15 @@ type MangaCardProps = {
 
 const MangaCard = ({ manga }: MangaCardProps) => {
     return (
-        <MangaCardClickable manga={manga}>
-            <div className="flex flex-col bg-complementary h-full max-h-[500px] border border-4 border-primary rounded-lg cursor-pointer hover:border-angryred transition duration-300 ease-in-out">
-                <div className="relative w-full h-[384px]">
-                    <Image
-                        src={manga.images.jpg.large_image_url}
-                        alt={manga.title}
-                        fill
-                        className="object-cover"
-                        priority
-                        quality={90}
-                        sizes="(max-width: 768px) 100vw, (max-width: 1200px) 50vw, 256px"
-                    />
-                </div>
-
-                <div className="h-[20%] flex flex-col items-center justify-center text-white text-sm text-center p-2">
-                    <div className="text-lg font-bold truncate w-full">
-                        {manga.title}
-                    </div>
-
-                    <div className="text-sm text-gray-300">
-                        Publish Date: <span className="font-medium">{manga.published?.string ?? "N/A"}</span>
-                    </div>
-
-                    <div className="text-sm text-gray-200 line-clamp-2">
-                        {manga.synopsis}
-                    </div>
-                </div>
-            </div>
-        </MangaCardClickable>
+        <ClickableCard data={manga} >
+            <Card 
+                imageSrc={manga.images.jpg.large_image_url}
+                title={manga.title}
+                releaseDate={manga.published?.string ?? "N/A"}
+                synopsis={manga.synopsis}
+                altText={manga.title}
+            />
+        </ClickableCard>
     );
 };
 
